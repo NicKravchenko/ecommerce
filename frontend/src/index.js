2cb1f5029083cc2c69734a9d0e9a6c16e29607cd
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-12-29 21:08:03 -0400

    Continue working on Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './bootstrap.min.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from "./store";
+import "./index.css";
+import "./bootstrap.min.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
